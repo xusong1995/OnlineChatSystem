d71861ec436ff290edcf8418ee92e367d2b361f1
XuSong <715173527@qq.com>
2018-11-24 11:29:05 +0800

    java练习小项目----在线聊天系统

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -12,7 +12,9 @@ import java.net.Socket;
  * @Data: Created on 2018-11-20 22:27
  */
 public class ChatServer {
+    //表示server有没有起来，有没有监听好，初始值为false
     boolean started = false;
+    //服务器端监听的socket
     ServerSocket ss = null;
 
     public static void main(String[] args) {
@@ -21,9 +23,9 @@ public class ChatServer {
 
     public void start() {
         try {
-            //创建绑定到指定端口(8888)的服务器套接字。
+            //创建绑定到指定端口(8888)的服务器套接字，表示监听在8888端口
             ss = new ServerSocket(8888);
-            //将状态设为true
+            //将状态设为true，表示已经启动server
             started = true;
             //处理端口已被占用的异常
         } catch (BindException e) {
@@ -38,7 +40,9 @@ public class ChatServer {
         try {
             //需要接受很多个客户端
             while (started) {
+                //只要已经启动，就不断去接收客户端的连接
                 Socket s = ss.accept();
+                //接收进来以后创建一个单独的线程
                 Client c = new Client(s);
                 //连接成功后输出信息
                 System.out.println("客户端已连接");
@@ -58,8 +62,11 @@ public class ChatServer {
 }
 
 class Client implements Runnable {
+    //包装了和每一个客户端的单独的连接
     private Socket s;
+    //从socket里面读内容的输入管道
     private DataInputStream dis = null;
+    //代表是否已经连接上
     private boolean bConnected = false;
 
 
